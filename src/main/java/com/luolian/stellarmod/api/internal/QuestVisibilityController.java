378e927a8168189f3911a6078c152a578266adc0
GRAINALCOHOL <132149646+GRAINALCOHOL@users.noreply.github.com>
2026-05-23 01:12:17 +0800

    2026-05-23 refactor: 移除了本模组对FTB和Origins的依赖和修改

--- src/main/java/com/luolian/stellarmod/api/internal/QuestVisibilityController.java
+++
@@ -1,6 +0,0 @@
-package com.luolian.stellarmod.api.internal;
-
-public interface QuestVisibilityController {
-    void stellar$setAlwaysInvisible(boolean alwaysInvisible);
-    boolean stellar$isAlwaysInvisible();
-}
